0bcc8f27720163692cf53b9f16af7250968e058b
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-09-02 10:41:18 +0400

    Works now!!!

--- build.zig
+++ build.zig
@@ -11,23 +11,37 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    exe.use_lld = true;
-
-    //exe.linkLibC();
+    exe.linkLibC();
     exe.linkLibCpp();
     exe.addIncludePath(b.path("include"));
     exe.linkSystemLibrary("d3d11");
-    exe.addObjectFile(b.path("lib/SDL2.dll"));
+    exe.linkSystemLibrary("d3d12");
+    exe.linkSystemLibrary("opengl32");
+    exe.linkSystemLibrary("shell32");
+    exe.linkSystemLibrary("kernel32");
+    exe.linkSystemLibrary("gdi32");
+    exe.linkSystemLibrary("user32");
+    exe.linkSystemLibrary("winmm");
+    exe.linkSystemLibrary("dxguid");
+    exe.linkSystemLibrary("shlwapi");
+    exe.linkSystemLibrary("ole32");
+    exe.linkSystemLibrary("shcore");
+    exe.linkSystemLibrary("avrt");
+    exe.linkSystemLibrary("ksuser");
+    exe.linkSystemLibrary("dbghelp");
 
-    exe.addLibraryPath(b.path("lib/"));
+    // TODO: Change to dynamically adding path
+    exe.addLibraryPath(b.path("lib/win"));
+    exe.addObjectFile(b.path("lib/win/SDL2.dll"));
     exe.linkSystemLibrary("bxRelease");
     exe.linkSystemLibrary("bimgRelease");
     exe.linkSystemLibrary("bgfxRelease");
 
-    exe.linkSystemLibrary("kernel32");
-    exe.linkSystemLibrary("gdi32");
-    exe.linkSystemLibrary("user32");
+    exe.linkSystemLibrary("mingw32");
+
+    //exe.linkSystemLibrary("bxRelease");
+    //exe.linkSystemLibrary("bimgRelease");
+    //exe.linkSystemLibrary("bgfxRelease");
 
     b.installArtifact(exe);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,39 @@
 const std = @import("std");
-const bgfx = @import("bgfx.zig");
+
+const c = @cImport({
+    @cInclude("stdlib.h");
+});
+
+usingnamespace @cImport({
+    @cInclude("GL/gl.h");
+    @cInclude("windows.h");
+    @cInclude("stdio.h");
+    @cInclude("unistd.h");
+    @cInclude("errno.h");
+    @cInclude("time.h");
+    @cInclude("string.h");
+});
+
 const sdl = @cImport({
     @cInclude("SDL2/SDL.h");
     @cInclude("SDL2/SDL_syswm.h");
     @cInclude("SDL2/SDL_version.h");
 });
 
-const WNDW_WIDTH = 1280;
-const WNDW_HEIGHT = 720;
+const bgfx = @import("bgfx.zig");
+
+const WNDW_WIDTH = 800;
+const WNDW_HEIGHT = 600;
 
 pub fn main() !void {
-    std.debug.print("Hello, World! \n", .{});
 
     // bgfx.bgfx_init();
     // bgfx.bgfx_reset(800, 600, bgfx.BGFX_RESET_VSYNC);
 
     // bgfx.bgfx_set_view_clear(0, bgfx.BGFX_CLEAR_COLOR | bgfx.BGFX_CLEAR_DEPTH, 0x303030ff, 1.0, 0);
 
-    const status = sdl.SDL_Init(sdl.SDL_INIT_VIDEO);
+    const status = sdl.SDL_Init(0);
+
     if (status != 0) {
         std.debug.print("Status: {d}\n", .{status});
         sdl.SDL_Log("Unable to init SDL: %s\n", sdl.SDL_GetError());
@@ -25,57 +41,77 @@ pub fn main() !void {
     defer sdl.SDL_Quit();
 
     const window = sdl.SDL_CreateWindow(
-        "My Game",
+        "BGFX",
         sdl.SDL_WINDOWPOS_UNDEFINED,
         sdl.SDL_WINDOWPOS_UNDEFINED,
         WNDW_WIDTH,
         WNDW_HEIGHT,
-        sdl.SDL_WINDOW_OPENGL,
-    );
+        0,
+    ).?;
 
     defer sdl.SDL_DestroyWindow(window);
 
     //var pd = bgfx.PlatformData{};
 
-    var wmi: sdl.SDL_SysWMinfo = undefined;
+    var wmi = std.mem.zeroes(sdl.SDL_SysWMinfo);
     wmi.version.major = sdl.SDL_MAJOR_VERSION;
     wmi.version.minor = sdl.SDL_MINOR_VERSION;
     wmi.version.patch = sdl.SDL_PATCHLEVEL;
 
-    if (sdl.SDL_GetWindowWMInfo(window, &wmi) == sdl.SDL_FALSE) {
+    const wmi_status = sdl.SDL_GetWindowWMInfo(window, &wmi);
+    std.debug.print("WMI Status: {d}\n", .{wmi_status});
+    if (wmi_status == sdl.SDL_FALSE) {
         std.debug.panic("Could not set window wm info\n", .{});
     }
 
-    var pd = std.mem.zeroes(bgfx.PlatformData);
-
-    pd.ndt = null;
-    pd.nwh = wmi.info.win.window;
-
-    pd.context = null;
-    pd.backBuffer = null;
-    pd.backBufferDS = null;
+    const pd = bgfx.PlatformData{
+        .ndt = c.NULL,
+        .nwh = wmi.info.win.window,
+        .context = c.NULL,
+        .backBuffer = c.NULL,
+        .backBufferDS = c.NULL,
+        .type = bgfx.NativeWindowHandleType.Default,
+    };
 
     bgfx.setPlatformData(&pd);
 
-    var init = std.mem.zeroes(bgfx.Init);
-    init.type = bgfx.RendererType.Count;
+    var renderers: [8]bgfx.RendererType = undefined;
+    const size = bgfx.getSupportedRenderers(8, &renderers);
+
+    std.debug.print("Available Renderers:\n", .{});
+    for (0..size) |i| {
+        std.debug.print("\t{d}: {s}\n", .{ i + 1, bgfx.getRendererName(renderers[i]) });
+    }
+
+    var init = std.mem.zeroInit(bgfx.Init, .{});
+    init.resolution.format = bgfx.TextureFormat.RGBA8;
     init.resolution.width = WNDW_WIDTH;
     init.resolution.height = WNDW_HEIGHT;
     init.resolution.reset = bgfx.ResetFlags_Vsync;
+    init.platformData = pd;
+    init.debug = true;
+    init.type = bgfx.RendererType.Count;
+
     const success = bgfx.init(&init);
     defer bgfx.shutdown();
+
     if (!success) {
         std.debug.panic("Could not start bgfx\n", .{});
     }
+    std.debug.print("Using renderer: {s}", .{bgfx.getRendererName(bgfx.getRendererType())});
 
-    bgfx.setViewClear(0, 0x0001 | 0x0002, 0x443355FF, 1.0, 0);
+    bgfx.setDebug(bgfx.DebugFlags_Text);
+
+    bgfx.setViewClear(0, bgfx.ClearFlags_Color | bgfx.ClearFlags_Depth, 0x443355FF, 1.0, 0);
     bgfx.setViewRect(0, 0, 0, WNDW_WIDTH, WNDW_HEIGHT);
 
     var frame_number: u64 = 0;
 
     while (true) {
+        var _event: sdl.SDL_Event = undefined;
+        while (sdl.SDL_PollEvent(&_event) > 0) {}
         bgfx.setViewRect(0, 0, 0, WNDW_WIDTH, WNDW_HEIGHT);
         bgfx.touch(0);
-        frame_number = bgfx.frame(false);
+        frame_number = bgfx.frame(true);
     }
 }
