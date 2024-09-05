7b34d144b67afd4c37e0446189bc63e38d3f5737
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-09-05 08:23:32 +0400

    Added mkdir to script otherwise it wouldn't work

--- compile.bat
+++ compile.bat
@@ -1,2 +1,3 @@
+mkdir src/compiled
 start shadercRelease -f shaders/vs_triangle.sc -o src/compiled/vs_triangle --type v -p s_5_0 -i include/bgfx
 start shadercRelease -f shaders/fs_triangle.sc -o src/compiled/fs_triangle --type f -p s_5_0 -i include/bgfx
