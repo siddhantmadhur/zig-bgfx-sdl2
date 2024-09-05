310778f7d7e5566743e6719e84f3e054a42d26a8
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-09-05 09:21:52 +0400

    Added SDL

--- build.zig
+++ build.zig
@@ -15,36 +15,38 @@ pub fn build(b: *std.Build) void {
     exe.linkLibCpp();
     exe.addIncludePath(b.path("include"));
 
-    exe.linkSystemLibrary("opengl32");
-    exe.linkSystemLibrary("shell32");
-    exe.linkSystemLibrary("kernel32");
-    exe.linkSystemLibrary("gdi32");
-    exe.linkSystemLibrary("user32");
-    exe.linkSystemLibrary("winmm");
-    exe.linkSystemLibrary("dxguid");
-    exe.linkSystemLibrary("shlwapi");
-    exe.linkSystemLibrary("ole32");
-    exe.linkSystemLibrary("shcore");
-    exe.linkSystemLibrary("avrt");
-    exe.linkSystemLibrary("ksuser");
-    exe.linkSystemLibrary("dbghelp");
-
-    if (target.result.os.tag == .windows) {
-        exe.linkSystemLibrary("d3d11");
-        exe.linkSystemLibrary("d3d12");
-        exe.addLibraryPath(b.path("lib/win/bgfx/"));
-        exe.addLibraryPath(b.path("lib/win/SDL/"));
-    } else {
-        std.debug.print("\nThis project currently only has libraries for Windows. \nTry compiling the libraries for the platform of your choice and change the build file.\n\n", .{});
-        std.process.exit(1);
+    switch (target.result.os.tag) {
+        .windows => {
+            std.debug.print("Building for windows...\n", .{});
+            exe.linkSystemLibrary("opengl32");
+            exe.linkSystemLibrary("shell32");
+            exe.linkSystemLibrary("kernel32");
+            exe.linkSystemLibrary("gdi32");
+            exe.linkSystemLibrary("user32");
+            exe.linkSystemLibrary("winmm");
+            exe.linkSystemLibrary("dxguid");
+            exe.linkSystemLibrary("shlwapi");
+            exe.linkSystemLibrary("ole32");
+            exe.linkSystemLibrary("shcore");
+            exe.linkSystemLibrary("avrt");
+            exe.linkSystemLibrary("ksuser");
+            exe.linkSystemLibrary("dbghelp");
+            exe.linkSystemLibrary("d3d11");
+            exe.linkSystemLibrary("d3d12");
+            exe.addLibraryPath(b.path("lib/win/bgfx/"));
+            exe.addLibraryPath(b.path("lib/win/SDL/"));
+            exe.linkSystemLibrary("SDL2");
+        },
+        .macos => {
+            exe.addSystemFrameworkPath(.{ .cwd_relative = "/Library/Frameworks" });
+            exe.addRPath(.{ .cwd_relative = "/Library/Frameworks" });
+            exe.linkFramework("SDL2");
+        },
+        else => {
+            std.debug.print("\nThis project does not contain static libraries for your operating system.\nManually setup the libraries and link them in the build.zig file and they should work.\n\n", .{});
+            std.process.exit(1);
+        },
     }
-
-    exe.linkSystemLibrary("SDL2");
-    exe.linkSystemLibrary("SDL2main");
-
-    exe.linkSystemLibrary("bxRelease");
-    exe.linkSystemLibrary("bimgRelease");
-    exe.linkSystemLibrary("bgfxRelease");
 
     //exe.linkSystemLibrary("bxRelease");
     //exe.linkSystemLibrary("bimgRelease");
